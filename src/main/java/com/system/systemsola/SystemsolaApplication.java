0bb226c33ad586e9ae3e3c8dc8ffbde45a03d96c
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-04-23 06:02:33 -0300

    Iniciando projeto de mapeamento do sistema solar

---
+++ src/main/java/com/system/systemsola/SystemsolaApplication.java
@@ -0,0 +1,13 @@
+package com.system.systemsola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemsolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemsolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/systemsola/SystemsolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.systemsola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemsolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

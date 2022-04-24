e14e9671cffaf2bb826fa8ab13f455beaf7dfde4
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-04-24 03:21:21 -0300

    Criando repository da tabela saturno

---
+++ src/main/java/com/system/systemsola/repository/RepositorySaturn.java
@@ -0,0 +1,9 @@
+package com.system.systemsola.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.system.systemsola.model.Saturn;
+
+public interface RepositorySaturn extends JpaRepository<Saturn, Long>{
+
+}

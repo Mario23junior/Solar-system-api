1b28ec4af3667e02c487d3aca09b9c8c25f1f228
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-04-24 04:14:19 -0300

    Criando controller

---
+++ src/main/java/com/system/systemsola/controller/ControllerMars.java
@@ -0,0 +1,27 @@
+package com.system.systemsola.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.system.systemsola.dto.MarsDTO;
+import com.system.systemsola.service.ServiceMars;
+
+@RestController
+@RequestMapping("/api/v1/planetary/mars/")
+public class ControllerMars {
+
+	private ServiceMars service;
+
+	public ControllerMars(ServiceMars service) {
+ 		this.service = service;
+	}
+
+	@PostMapping
+	public ResponseEntity<MarsDTO> save(@RequestBody MarsDTO MarsDto) {
+		return service.saveBody(MarsDto);
+	}
+
+}

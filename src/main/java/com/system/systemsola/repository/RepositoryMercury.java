948154bcf17b114b1c7badfac198f679e5d5625c
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-04-24 01:40:30 -0300

    Criando repository de Mercury

---
+++ src/main/java/com/system/systemsola/repository/RepositoryMercury.java
@@ -0,0 +1,9 @@
+package com.system.systemsola.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.system.systemsola.model.Mercury;
+
+public interface RepositoryMercury extends JpaRepository<Mercury, Long>{
+
+}

495ff1acf73c1404dad95905b551fabaa41906f4
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-04-24 01:02:15 -0300

    Listando informações por id

--- src/main/java/com/system/systemsola/controller/ControllerSolarSystem.java
+++ src/main/java/com/system/systemsola/controller/ControllerSolarSystem.java
@@ -1,12 +1,15 @@
 package com.system.systemsola.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.system.systemsola.dto.SystemSolarDTO;
+import com.system.systemsola.model.SystemSolar;
 import com.system.systemsola.service.ServiceSolarSystem;
 
 @RestController
@@ -24,4 +27,10 @@ public class ControllerSolarSystem {
 	public ResponseEntity<SystemSolarDTO> save(@RequestBody SystemSolarDTO systemSolarDto) {
 		return service.saveBody(systemSolarDto);
 	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<SystemSolar> listId(@PathVariable Long id) {
+		return service.listId(id);
+	}
+	
 }
--- src/main/java/com/system/systemsola/service/ServiceSolarSystem.java
+++ src/main/java/com/system/systemsola/service/ServiceSolarSystem.java
@@ -1,5 +1,7 @@
 package com.system.systemsola.service;
 
+import java.util.Optional;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -41,4 +43,14 @@ public class ServiceSolarSystem {
 			throw new ReturnErroFindSearchNotFound("Informação "+ systemFind.getName()+" Já esta cadastrada");
 		}
 	}
+	
+	public ResponseEntity<SystemSolar> listId(Long id) {
+		Optional<SystemSolar> idPla = repoSystemSol.findById(id);
+		if(idPla.isPresent()) {
+			return ResponseEntity.ok(mapper.map(idPla.get(), SystemSolar.class));
+		} else {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+	}
+	
 }

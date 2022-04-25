eeb2fa96321f8e90e679120ea7404a62c72ef80c
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-04-25 00:07:33 -0300

    Criando repository da entidade venus

---
+++ src/main/java/com/system/systemsola/repository/RepositoryVenus.java
@@ -0,0 +1,9 @@
+package com.system.systemsola.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.system.systemsola.model.Venus;
+
+public interface RepositoryVenus extends JpaRepository<Venus, Long>{
+
+}
